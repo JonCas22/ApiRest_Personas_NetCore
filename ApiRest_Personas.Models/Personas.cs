90a7321dd0d45e3d973e66cef926d0c98c01037b
JonCas22 <36029286+JonCas22@users.noreply.github.com>
2020-03-27 09:43:59 +0100

    Add ApiRest_Personas.Models

---
+++ ApiRest_Personas.Models/Personas.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ApiRest_Personas.Models
+{
+    public partial class Personas
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string City { get; set; }
+        public int Phone { get; set; }
+        public string Province { get; set; }
+    }
+}
---
+++ ApiRest_Personas.Models/Users.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiRest_Personas.Models
+{
+    public class Users
+    {
+        public long Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Rol { get; set; }
+        //public DateTime Date { get; set; }
+    }
+}
