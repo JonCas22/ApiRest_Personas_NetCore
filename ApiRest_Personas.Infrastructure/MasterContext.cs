d6cabed16fb9ecb009385dcac17fe4fab690fdbe
JonCas22 <36029286+JonCas22@users.noreply.github.com>
2020-03-27 09:45:06 +0100

    Add ApiRest_Personas.Infrastructure

---
+++ ApiRest_Personas.Infrastructure/MasterContext.cs
@@ -0,0 +1,94 @@
+﻿using ApiRest_Personas.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Prueba.Models
+{
+    public partial class MasterContext : DbContext
+    {
+        public MasterContext()
+        {
+        }
+
+        public MasterContext(DbContextOptions<MasterContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Personas> Personas { get; set; }
+
+        public virtual DbSet<Users> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Personas>(entity =>
+            {
+                entity.ToTable("personas");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Age).HasColumnName("age");
+
+                entity.Property(e => e.City)
+                    .IsRequired()
+                    .HasColumnName("city")
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasColumnName("name")
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Phone).HasColumnName("phone");
+                entity.Property(e => e.Province).HasColumnName("province")
+                    .IsRequired()
+                    .HasColumnName("province")
+                    .HasMaxLength(100)
+                    .IsUnicode(false); ;
+            });
+            
+            //Users modelBuilder
+            
+            modelBuilder.Entity<Users>(entity =>
+            {
+                entity.ToTable("users");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.FirstName)
+                    .IsRequired()
+                    .HasColumnName("firstname")
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.LastName)
+                    .IsRequired()
+                    .HasColumnName("lastname")
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Username).HasColumnName("username")
+                    .IsRequired()
+                    .HasColumnName("username")
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Password).HasColumnName("password")
+                    .IsRequired()
+                    .HasColumnName("password")
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+                entity.Property(e => e.Rol).HasColumnName("rol")
+                    .IsRequired()
+                    .HasColumnName("rol")
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
