cb0a2afeae0d6d2beee4d6019139f9824aab855c
JonCas22 <36029286+JonCas22@users.noreply.github.com>
2020-03-27 09:45:59 +0100

    ApiRest_Personas.Application

---
+++ ApiRest_Personas.Application/IPersonaService.cs
@@ -0,0 +1,23 @@
+﻿using ApiRest_Personas.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiRest_Personas.Application
+{
+    public interface IPersonaService
+    {
+        Task<IEnumerable<Personas>> GetAllPersonas();
+
+        Task<Personas> GetPersonaById(long id);
+
+        Task<Personas> PutPersona(long id, Personas personas);
+
+        Task<Personas> PostPersona(Personas personas);
+
+        Task<Personas> DeletePersona(long id);
+
+        bool PersonasExists(long id);
+    }
+}
---
+++ ApiRest_Personas.Application/IUserService.cs
@@ -0,0 +1,21 @@
+﻿using ApiRest_Personas.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ApiRest_Personas.Application
+{
+    public interface IUserService
+    {
+        Task<IEnumerable<Users>> GetAllUsers();
+
+        Task<Users> GetUserById(long id);
+
+        Task<Users> PutUser(long id, Users users);
+
+        Task<Users> PostUser(Users users);
+
+        Task<Users> DeleteUser(long id);
+
+        bool UsersExists(long id);
+    }
+}
